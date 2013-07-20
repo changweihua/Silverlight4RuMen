7d5a92a8a0d77596e3f0343da0828d8d3ff2d79f
Changweihua <changweihua@outlook.com>
2013-07-20 10:29:32 +0800

    第六章第二次提交

--- ch6_1/MainPage.xaml.cs
+++ ch6_1/MainPage.xaml.cs
@@ -9,14 +9,58 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ch6_1
 {
     public partial class MainPage : UserControl
     {
+        DispatcherTimer vidTimer = new DispatcherTimer();
+
         public MainPage()
         {
             InitializeComponent();
+            
+        }
+
+        void vidTimer_Tick(object sender, EventArgs e)
+        {
+            slider1.Value = mediaElement1.Position.Seconds;
+        }
+
+        private void mediaElement1_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            slider1.Maximum = mediaElement1.NaturalDuration.TimeSpan.Ticks;
+
+            TimelineMarker t = new TimelineMarker();
+            t.Text = "这是动态添加的记号";
+            t.Time = new TimeSpan(0, 0, 22);
+            mediaElement1.Markers.Add(t);
+
+            vidTimer.Interval = new TimeSpan(0, 0, 0, 1);
+            vidTimer.Tick += new EventHandler(vidTimer_Tick);
+            vidTimer.Start();
+
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement1.Play();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement1.Pause();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            mediaElement1.Stop();
+        }
+
+        private void mediaElement1_MarkerReached(object sender, TimelineMarkerRoutedEventArgs e)
+        {
+            label1.Text = e.Marker.Text;
         }
     }
 }
---
+++ ch6_2/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ch6_2
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            bool flag = CaptureDeviceConfiguration.RequestDeviceAccess();
+
+            if (flag)
+            {
+                CaptureSource cs = new CaptureSource 
+                { 
+                    VideoCaptureDevice = CaptureDeviceConfiguration.GetDefaultVideoCaptureDevice()
+                };
+
+                VideoBrush brush = new VideoBrush();
+                brush.SetSource(cs);
+
+                cam.Fill = brush;
+                cs.Start();
+
+            }
+
+        }
+    }
+}
+
+
