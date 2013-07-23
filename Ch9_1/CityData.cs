37f3456a3f39656428cb3f84cd6c8319e15b831e
Changweihua <changweihua@outlook.com>
2013-07-23 18:05:58 +0800

    第九章 第一次提交

---
+++ Ch9_1/CityData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Ch9_1
+{
+    #region 作者和版权
+    /*************************************************************************************
+     * CLR 版本:       4.0.30319.18034
+     * 类 名 称:       CityData
+     * 机器名称:       LUMIA800
+     * 命名空间:       Ch9_1
+     * 文 件 名:       CityData
+     * 创建时间:       2013/7/23 17:13:05
+     * 作    者:       常伟华 Changweihua
+	 * 版    权:	   CityData说明：本代码版权归常伟华所有，使用时必须带上常伟华网站地址 All Rights Reserved (C) 2013 - 2013
+     * 签    名:       To be or not, it is not a problem !
+     * 网    站:       http://www.cmono.net
+     * 邮    箱:       changweihua@outlook.com  
+     * 唯一标识：	   5d1ee116-5528-4c84-a40e-0f6af741ac54  
+	 *
+	 * 登录用户:       Changweihua
+	 * 所 属 域:       Lumia800
+
+	 * 创建年份:       2013
+     * 修改时间:
+     * 修 改 人:
+     * 
+     ************************************************************************************/
+    #endregion
+
+    /// <summary>
+    /// 摘要
+    /// </summary>
+    public class CityData
+    {
+        public string CityName { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public CityData(string cityName, double latitude, double longitude)
+        {
+            this.CityName = cityName;
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+        }
+
+    }
+}
---
+++ Ch9_1/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Browser;
+
+namespace Ch9_1
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            UpdateCities("德国");
+
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //向浏览器注册为 “可脚本处理”
+            HtmlPage.RegisterScriptableObject("MySilverlightObject", this);
+        }
+
+        private List<CityData> getCities(string country)
+        {
+            List<CityData> cities = new List<CityData>();
+
+            switch (country)
+            {
+                case "法国":
+                    {
+                        cities.Add(new CityData("巴黎", 48.87, 2.33));
+                        cities.Add(new CityData("卢尔德", 43.1, 0.05));
+                        cities.Add(new CityData("图卢兹", 43.6, 1.38));
+                        break;
+                    }
+                case "英国":
+                    {
+                        cities.Add(new CityData("伦敦", 51.5, 0));
+                        cities.Add(new CityData("斯特拉特福", 52.3, -1.71));
+                        cities.Add(new CityData("爱丁堡", 55.95, -3.16));
+                        break;
+                    }
+                case "德国":
+                    {
+                        cities.Add(new CityData("柏林", 52.52, 13.42));
+                        cities.Add(new CityData("慕尼黑", 48.13, 11.57));
+                        cities.Add(new CityData("汉堡", 53.58, 9.98));
+                        break;
+                    }
+                default:
+                    break;
+            }
+
+            return cities;
+        }
+
+        [ScriptableMember]//公开函数
+        public void UpdateCities(string country)
+        {
+            List<CityData> myCities = getCities(country);
+            itmCities.ItemsSource = myCities;
+        }
+
+    }
+}
